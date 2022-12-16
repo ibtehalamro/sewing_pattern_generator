7cf1c8b9e05769dffbf51319756037c2c514d6e0
ibtehal <ibtehalamro11@gmail.com>
2022-12-16 02:14:09 +0200

    SP-5 setup for sleeve drafting.

--- scripts/Manager.js
+++ scripts/Manager.js
@@ -4,6 +4,7 @@ import { Pattern } from "./Pattern.js";
 import { Point } from "./Point.js";
 import { Line } from "./Line.js";
 import { Curve } from "./Curve.js";
+
 let neckCirc = 0;
 let shoulderWidth = 0;
 let armHoleCirc = 0;
@@ -13,6 +14,8 @@ let waistToShoulderLengthBack = 0;
 let waistCirc = 0;
 let bustSpan = 0;
 let bustDepth = 0;
+let womenFrontSleeveLength = 0;
+let womenBackSleeveLength = 0;
 (() => {
   resetSvg();
   entryPoint();
@@ -49,6 +52,9 @@ function manageButtons() {
   document
     .querySelector("#generate_back_bodice")
     .addEventListener("click", () => generateBackBodicePattern());
+  document
+    .querySelector("#generate_sleeve_pattern")
+    .addEventListener("click", () => generateWomenSleevePattern());
 }
 
 function getMeasurements() {
@@ -56,11 +62,21 @@ function getMeasurements() {
   shoulderWidth = parseFloat(document.querySelector("#shoulder_width").value);
   armHoleCirc = parseFloat(document.querySelector("#armhole_circ").value);
   bustWidth = parseFloat(document.querySelector("#bust_circ").value);
-  waistToShoulderLength = parseFloat(document.querySelector("#shoulder_to_waist").value);
-  waistToShoulderLengthBack = parseFloat(document.querySelector("#shoulder_to_waist_back").value);
+  waistToShoulderLength = parseFloat(
+    document.querySelector("#shoulder_to_waist").value
+  );
+  waistToShoulderLengthBack = parseFloat(
+    document.querySelector("#shoulder_to_waist_back").value
+  );
   waistCirc = parseFloat(document.querySelector("#waist_circ").value);
   bustSpan = parseFloat(document.querySelector("#bust_span").value);
   bustDepth = parseFloat(document.querySelector("#bust_depth").value);
+  womenFrontSleeveLength = parseFloat(
+    document.querySelector("#women_front_sleeve_length").value
+  );
+  womenBackSleeveLength = parseFloat(
+    document.querySelector("#women_back_sleeve_length").value
+  );
 }
 
 function generateFrontBodicePattern() {
@@ -79,12 +95,12 @@ function generateFrontBodicePattern() {
   const point7 = new Point(getBustWidth(), point5.getY());
   const point8 = new Point(point1.getX(), waistToShoulderLength);
   const point9 = new Point(getWaistWidth(), point8.getY());
-  const point10 = new Point(point1.getX(), point1.getY()  +getNeckWidth() + 1);
+  const point10 = new Point(point1.getX(), point1.getY() + getNeckWidth() + 1);
   const point11 = new Point(point2.getX(), point10.getY());
   const point12 = new Point(bustSpan, waistToShoulderLength);
   const point13 = getIntersectPoint();
-  const point14 = new Point( bustSpan , point13.getY()+2.5);
-  const point15 = getMiddlePoint(point7 , point9);//middle point
+  const point14 = new Point(bustSpan, point13.getY() + 2.5);
+  const point15 = getMiddlePoint(point7, point9); //middle point
   const point16 = getPointAwayOnLine(point13, point15);
   const point17 = getPointAwayOnLine(point15, point7);
   const point18 = getPointAwayOnLine(point17, point7);
@@ -99,12 +115,17 @@ function generateFrontBodicePattern() {
   const neckLineVerticalLine = new Line(point11, point2);
   const bustDepthLine = new Line(point10, point13);
   const frontDartLine = new Line(point12, point13);
-  const frontDartLeftLegLine = new Line(point14, new Point(point12.getX()-1.5,point12.getY()));
-  const frontDartRightLegLine = new Line(point14, new Point(point12.getX()+1.5,point12.getY()));
+  const frontDartLeftLegLine = new Line(
+    point14,
+    new Point(point12.getX() - 1.5, point12.getY())
+  );
+  const frontDartRightLegLine = new Line(
+    point14,
+    new Point(point12.getX() + 1.5, point12.getY())
+  );
   const sideDartLowerLine = new Line(point13, point15);
-  const sideDartMiddlePoint = new Line(point16 , point17);
-  const sideDartUpperPoint = new Line(point16 , point18);
-
+  const sideDartMiddlePoint = new Line(point16, point17);
+  const sideDartUpperPoint = new Line(point16, point18);
 
   const pattern = new Pattern(neckLineLowerLine);
   pattern.addLine(neckLineVerticalLine);
@@ -120,7 +141,7 @@ function generateFrontBodicePattern() {
   pattern.addLine(frontDartLine);
   pattern.addLine(frontDartLeftLegLine);
   pattern.addLine(frontDartRightLegLine);
-  pattern.addLine(sideDartLowerLine); 
+  pattern.addLine(sideDartLowerLine);
   pattern.addLine(sideDartMiddlePoint);
   pattern.addLine(sideDartUpperPoint);
 
@@ -129,8 +150,17 @@ function generateFrontBodicePattern() {
   console.log(pointsString);
   document.querySelector("#polyLine").setAttribute("points", pointsString);
 
-  new Curve().threePointCurveFromPointsArray([point10.getPointAsArray(),[point2.getX(), point10.getY()] , point2.getPointAsArray()]);
-  new Curve().fourPointCurveFromPointsArray([point4.getPointAsArray(),  [point6.getX()-2,point6.getY()], [point6.getX(),point5.getY()+1], point7.getPointAsArray()]);
+  new Curve().threePointCurveFromPointsArray([
+    point10.getPointAsArray(),
+    [point2.getX(), point10.getY()],
+    point2.getPointAsArray(),
+  ]);
+  new Curve().fourPointCurveFromPointsArray([
+    point4.getPointAsArray(),
+    [point6.getX() - 2, point6.getY()],
+    [point6.getX(), point5.getY() + 1],
+    point7.getPointAsArray(),
+  ]);
 }
 
 function generateBackBodicePattern() {
@@ -138,21 +168,23 @@ function generateBackBodicePattern() {
   //not all points will be added to the pattern just the ones that will drawn
   getMeasurements();
   const point1 = new Point(0, waistToShoulderLengthBack);
-  const point2 = new Point(point1.getX() ,2);
+  const point2 = new Point(point1.getX(), 2);
   const point3 = new Point(point1.getX() + getNeckWidth(), point2.getY());
   const point4 = new Point(point3.getX(), 0);
   const point5 = new Point(getShoulderWidth(), point4.getY());
-  const point6 = new Point( point5.getX(), point5.getY() + getShoulderSlope());
-  const point7 = new Point(point6.getX(), point6.getY()+ getArmHoleHeight());
-  const point8 = new Point(point7.getX()-1,  point6.getY()+ (getArmHoleHeight()/2));
-  const point9 = new Point(bustWidth/4, point7.getY());
-  const point10 = new Point((waistCirc/4)+2, point1.getY() );
-  const point11 = new Point(point1.getX()+(point10.getX()/2), point1.getY());
-  const point12 = new Point(point11.getX(), point7.getY()+2.5);
-  const point13 = new Point(point11.getX()-1, point11.getY());
-  const point14 = new Point(point11.getX()+1, point11.getY());
-  const point15 = new Point( 0 , point9.getY());
-
+  const point6 = new Point(point5.getX(), point5.getY() + getShoulderSlope());
+  const point7 = new Point(point6.getX(), point6.getY() + getArmHoleHeight());
+  const point8 = new Point(
+    point7.getX() - 1,
+    point6.getY() + getArmHoleHeight() / 2
+  );
+  const point9 = new Point(bustWidth / 4, point7.getY());
+  const point10 = new Point(waistCirc / 4 + 2, point1.getY());
+  const point11 = new Point(point1.getX() + point10.getX() / 2, point1.getY());
+  const point12 = new Point(point11.getX(), point7.getY() + 2.5);
+  const point13 = new Point(point11.getX() - 1, point11.getY());
+  const point14 = new Point(point11.getX() + 1, point11.getY());
+  const point15 = new Point(0, point9.getY());
 
   const shoulderLine = new Line(point4, point6);
   const upperArmholeLine = new Line(point6, point8);
@@ -189,8 +221,88 @@ function generateBackBodicePattern() {
   console.log(pointsString);
   document.querySelector("#polyLine").setAttribute("points", pointsString);
 
-  new Curve().threePointCurveFromPointsArray([point2.getPointAsArray(),[point4.getX(), point2.getY()] , point4.getPointAsArray()]);
-  new Curve().fourPointCurveFromPointsArray([point6.getPointAsArray(),  [point8.getX()-2,point8.getY()], [point7.getX(),point7.getY()], point9.getPointAsArray()]);
+  new Curve().threePointCurveFromPointsArray([
+    point2.getPointAsArray(),
+    [point4.getX(), point2.getY()],
+    point4.getPointAsArray(),
+  ]);
+  new Curve().fourPointCurveFromPointsArray([
+    point6.getPointAsArray(),
+    [point8.getX() - 2, point8.getY()],
+    [point7.getX(), point7.getY()],
+    point9.getPointAsArray(),
+  ]);
+}
+
+function generateWomenSleevePattern() {
+  resetSvg();
+  //not all points will be added to the pattern just the ones that will drawn
+  getMeasurements();
+  const point1 = new Point(0, waistToShoulderLengthBack);
+  const point2 = new Point(point1.getX(), 2);
+  const point3 = new Point(point1.getX() + getNeckWidth(), point2.getY());
+  const point4 = new Point(point3.getX(), 0);
+  const point5 = new Point(getShoulderWidth(), point4.getY());
+  const point6 = new Point(point5.getX(), point5.getY() + getShoulderSlope());
+  const point7 = new Point(point6.getX(), point6.getY() + getArmHoleHeight());
+  const point8 = new Point(
+    point7.getX() - 1,
+    point6.getY() + getArmHoleHeight() / 2
+  );
+  const point9 = new Point(bustWidth / 4, point7.getY());
+  const point10 = new Point(waistCirc / 4 + 2, point1.getY());
+  const point11 = new Point(point1.getX() + point10.getX() / 2, point1.getY());
+  const point12 = new Point(point11.getX(), point7.getY() + 2.5);
+  const point13 = new Point(point11.getX() - 1, point11.getY());
+  const point14 = new Point(point11.getX() + 1, point11.getY());
+  const point15 = new Point(0, point9.getY());
+
+  const shoulderLine = new Line(point4, point6);
+  const upperArmholeLine = new Line(point6, point8);
+  const lowerArmholeLine = new Line(point8, point9);
+  const armpitToWaistLine = new Line(point9, point10);
+  const waistLine = new Line(point10, point1);
+  const centerFrontLine = new Line(point1, point2);
+  const neckLineLowerLine = new Line(point2, point3);
+  const neckLineVerticalLine = new Line(point3, point4);
+  const backDartLine = new Line(point11, point12);
+  const backDartLeftLegLine = new Line(point12, point13);
+  const backDartRightLegLine = new Line(point12, point14);
+  const chestLine = new Line(point15, point9);
+
+  const pattern = new Pattern(centerFrontLine);
+  pattern.addLine(neckLineLowerLine);
+  pattern.addLine(neckLineVerticalLine);
+
+  pattern.addLine(shoulderLine);
+
+  pattern.addLine(upperArmholeLine);
+  pattern.addLine(lowerArmholeLine);
+
+  pattern.addLine(armpitToWaistLine);
+  pattern.addLine(waistLine);
+
+  pattern.addLine(backDartLine);
+  pattern.addLine(backDartLeftLegLine);
+  pattern.addLine(backDartRightLegLine);
+  // pattern.addLine(chestLine);
+
+  const lines = pattern.getLines();
+  let pointsString = lines.join(" , ");
+  console.log(pointsString);
+  document.querySelector("#polyLine").setAttribute("points", pointsString);
+
+  new Curve().threePointCurveFromPointsArray([
+    point2.getPointAsArray(),
+    [point4.getX(), point2.getY()],
+    point4.getPointAsArray(),
+  ]);
+  new Curve().fourPointCurveFromPointsArray([
+    point6.getPointAsArray(),
+    [point8.getX() - 2, point8.getY()],
+    [point7.getX(), point7.getY()],
+    point9.getPointAsArray(),
+  ]);
 }
 function getNeckWidth() {
   return neckCirc / 5;
@@ -212,19 +324,18 @@ function getWaistWidth() {
 }
 
 function getIntersectPoint() {
- let y = Math.sqrt( Math.pow(bustDepth, 2)-Math.pow(bustSpan,2) );
-  return new Point(bustSpan,getNeckWidth()+1+y);
+  let y = Math.sqrt(Math.pow(bustDepth, 2) - Math.pow(bustSpan, 2));
+  return new Point(bustSpan, getNeckWidth() + 1 + y);
 }
 
-function getMiddlePoint(point1 , point2){
-  let middlePoint = new Draw().getLineMiddlePoint(point1,point2);
- 
-  return new Point(middlePoint.xt,middlePoint.yt);
+function getMiddlePoint(point1, point2) {
+  let middlePoint = new Draw().getLineMiddlePoint(point1, point2);
+
+  return new Point(middlePoint.xt, middlePoint.yt);
 }
 
-function getPointAwayOnLine(point1 , point2){
-  let point = new Draw().getPointOnLineAwayByDistance(2.5, point1,point2);
- 
-  return new Point(point.xt,point.yt);
-}
+function getPointAwayOnLine(point1, point2) {
+  let point = new Draw().getPointOnLineAwayByDistance(2.5, point1, point2);
 
+  return new Point(point.xt, point.yt);
+}
